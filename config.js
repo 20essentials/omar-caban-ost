c96eb9709ce91df090c4734bb1cb38f7067fd5bb
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:54:47 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'OMAR CABÁN';
+export const nameSongs = [
+	"RED SWAN", /*1*/
+	"SILHOUETTE", /*2*/
+	"KIBOU NO UTA", /*3*/
+	"ENAMEL", /*4*/
+	"ALUMINA", /*5*/
+	"7TH ELEMENT", /*6*/
+	"THE WORLD", /*7*/
+	"LET ME HEAR", /*8*/
+	"CLOSER", /*9*/
+	"UNRAVEL", /*10*/
+	"HEY KIDS", /*11*/
+	"SHIVER", /*12*/
+	"MASK", /*13*/
+	"PLACE TO TRY", /*14*/
+	"CHŌZETSU DYNAMIC", /*15*/
+	"ONE DAY", /*16*/
+	"DISTANCE", /*17*/
+	"SHARE THE WORLD", /*18*/
+	"YORU WA NEMURERU KAI?", /*19*/
+	"REQUIEM DER MORGENRÖTE", /*20*/
+]
